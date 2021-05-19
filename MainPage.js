4566ef60cc422619c7e6e5869933efa8a8bf7ffe
gfalkeenthros <58170723+gfalkeenthros@users.noreply.github.com>
2021-05-19 12:21:49 +0200

    addded customisable names to sensors

--- MainPage.js
+++ MainPage.js
@@ -1,4 +1,5 @@
 const baseUrl = "https://slikskabdata.azurewebsites.net/api/reading"
+
 Vue.createApp({
     data() {
         //var image = yourString.substring(2, yourString.length-1);
@@ -10,52 +11,77 @@ Vue.createApp({
             filtered: [],
             oldLength: -1,
             sensors: [],
-            isShutDown: false
+            isShutDown: false,
+            sensorNames: []
         }
     },
     methods: {
         getReadings() {
-            if(!this.isShutDown){
+            if (!this.isShutDown) {
 
-            this.oldLength = this.readings.length;
-            this.readings = [];
-            axios.get(baseUrl).then(function (response) {
-                for (reading of response["data"]) {
-                    var image = reading.image
-                    if (image == null) {
-                        image = "";
+                this.oldLength = this.readings.length;
+                this.readings = [];
+                axios.get(baseUrl).then(function (response) {
+                    for (reading of response["data"]) {
+                        var image = reading.image
+                        if (image == null) {
+                            image = "";
+                        }
+                        image = image.substring(2, image.length - 2)
+                        reading.image = image
+                        this.readings.push(reading)
+                        if (!this.sensors.includes(reading.sensorId)) {
+                            this.sensors.push(reading.sensorId);
+                            if(this.sensorNames[reading.sensorId] == undefined || this.sensorNames[reading.sensorId] == reading.sensorId-1){
+                                this.sensorNames[reading.sensorId-1] = reading.sensorId
+                            }
+                        }
                     }
-                    image = image.substring(2, image.length - 2)
-                    reading.image = image
-                    this.readings.push(reading)
-                    if(!this.sensors.includes(reading.sensorId)){
-                        this.sensors.push(reading.sensorId);
-                        
+
+                    this.filtered = this.readings.slice();
+                    if (this.readings.length > this.oldLength) {
+                        this.sendNotification("New Readings Available")
                     }
-                }
 
-                this.filtered = this.readings.slice();
-                if(this.readings.length > this.oldLength){
-                    this.sendNotification("New Readings Available")
-                }
-
-            }.bind(this))
+                }.bind(this))
             }
         },
-         statusShutOff() {
+
+        getName(index){
+            return this.sensorNames[index]
+        },
+
+        saveName(id){
+            var name = document.getElementById("nameInput" + id).value;
+            $cookies.set(id.toString(), name);
+            this.sensorNames[id-1] = name
+            this.updateNames()
+        },
+
+        updateNames(){
+            pairs = document.cookie.split(";")
+            for(p of pairs)
+            {
+                var pieces = p.split("=");
+                var index = parseInt(pieces[0])
+                var name = pieces[1]
+                this.sensorNames[index-1] = name
+            }
+        },
+
+        statusShutOff() {
             var x = document.getElementById("statusCheck");
             return x.innerHTML = "Systemet er slukket"
-          },
-          statusShutOn(){
-              var x = document.getElementById("statusCheck");
-              return x.innerHTML = "Systemet er tændt"
-          },
+        },
+        statusShutOn() {
+            var x = document.getElementById("statusCheck");
+            return x.innerHTML = "Systemet er tændt"
+        },
 
-        shutdown(){
+        shutdown() {
             this.isShutDown = true;
         },
-        turnmeon()
-        {
+        turnmeon() {
             this.isShutDown = false;
         },
         reset() {
@@ -92,30 +118,31 @@ Vue.createApp({
             }
         },
 
-        describeBool(value){
-            if(value == true){
+        describeBool(value) {
+            if (value == true) {
                 return "Ja"
             }
-            else{
+            else {
                 return "Nej"
             }
         },
 
-        imageClickUnlock(id){
-            var element=document.getElementById(id)
-            element.src='Images/unlocked.gif'
-            
+        imageClickUnlock(id) {
+            var element = document.getElementById(id)
+            element.src = 'Images/unlocked.gif'
+
 
         },
-        imageClickLock(id){
-            var element=document.getElementById(id)
-            element.src='Images/locked.gif'
+        imageClickLock(id) {
+            var element = document.getElementById(id)
+            element.src = 'Images/locked.gif'
         }
     },
 
 
     created: function () {
         this.getReadings()
+        this.updateNames()
         this.interval = setInterval(() => this.getReadings(), 10000);
     }
 }).mount("#app")
